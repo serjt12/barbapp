1cef1282506c3d9a138c78c5d4928548ce7d3427
serjt12 <jandreus87@hotmail.com>
2024-08-25 08:15:31 -0500

    updating actions

--- backend/barbapp/settings.py
+++ backend/barbapp/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
